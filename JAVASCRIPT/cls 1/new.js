3f1ad601f06b97305a24e77aca3eaedb91fdb576
Poorni-03 <poornitc2003@gmail.com>
2025-03-12 09:32:07 +0530

    files are being are added

---
+++ JAVASCRIPT/cls 1/new.js
@@ -0,0 +1,4 @@
+console.log("code is running...")
+console.log("Hello World!")
+console.log("code is also running...")
+console.log("code is looking like a wow...")
---
+++ JAVASCRIPT/cls 1/script.js
@@ -0,0 +1,23 @@
+alert("Hello World !");
+
+
+console.log("code is running...")
+console.log("code is also running...")
+console.log("code is looking like a wow...")
+
+var a = prompt("Enter your number")
+console.log("Your number is "+a);
+
+var isTrue = confirm("Are you sure you want to leave this page and burn your computer")
+
+if(isTrue){
+    console.log("Computer is Blasting ");
+}
+else{
+    console.log("Computer is not Blasting")
+}
+
+
+document.title = "hey i am good"
+
+document.body.style.backgroundColor="red"
